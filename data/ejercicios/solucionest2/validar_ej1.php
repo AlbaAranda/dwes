6d0231d8b66a13a7a3c8dbea310772ae69164891
Alba Aranda <albaarandagomez@gmail.com>
2022-10-13 12:47:19 +0100

    modificacion ejercicios

--- data/ejercicios/solucionest2/validar_ej1.php
+++ data/ejercicios/solucionest2/validar_ej1.php
@@ -1,10 +1,44 @@
 <?php
-       if (isset($_GET['envio'])) {
+    if (isset($_GET['envio'])) {
         $titulo = $_GET['titulo'];
         if (!empty($titulo)) {
 
             echo "<br>El titulo del libro es: " . $titulo;
-        } else {
+        } 
+        else {
             echo "<br>No has elegido ningún titulo de libro";
+        }
+    }
+
+    if (isset($_GET['envio'])) {
+        $autor = $_GET['autor'];
+        if (!empty($autor)) {
+
+            echo "<br>El autor del libro es: " . $autor;
+        } 
+        else {
+            echo "<br>No has elegido ningún autor de libro";
+        }
+    }
+
+    if (isset($_GET['envio'])) {
+        $editorial = $_GET['editorial'];
+        if (!empty($editorial)) {
+
+            echo "<br>La editorial del libro es: " . $editorial;
+        } 
+        else {
+            echo "<br>No has elegido ninguna editorial de libro";
+        }
+    }
+
+    if (isset($_GET['envio'])) {
+        $paginas = $_GET['pag'];
+        if (!empty($paginas)) {
+
+            echo "<br>El libro tiene : " . $paginas . " páginas";
+        } 
+        else {
+            echo "<br>No has escrito el número de páginas del libro";
         }
     }

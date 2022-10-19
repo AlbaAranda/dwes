8f306c8515d3fa99823be6b3cc29ad37eb3c1078
Alba Aranda <albaarandagomez@gmail.com>
2022-10-19 22:24:51 +0100

    modificacion ejercicios

--- data/ejercicios/apuntes/cookies.php
+++ data/ejercicios/apuntes/cookies.php
@@ -7,20 +7,33 @@
 //strtotime();
 
 //DEFINIR UNA COOKIE
-//nombre, valor y duración
+
+//nombre, valor y duración (cuanto tiempo va a ser válida esa cookie)
+
 //se ponen siempren antes de mostrar nada en pantalla, ya que van en la cabecera
 
 // setcookie("primeracookie", "aceptado", time() + (24*60*60)); // la cookie dura un dia 
 
 // setcookie("primeracookie", "aceptado", strtotime("+1 week")); // la cookie dura hasta una semana
 
-$miarray = ["hola",25,[1,3,5]];
-//con serialize conviertes un array en una cadena 
-$infoguardar = serialize($miarray);
-setcookie("primeracookie", "aceptado", strtotime("+1 minute"));
-setcookie("segundacookie", $infoguardar, time()+600);
+$miarray = ['a'=>"hola",'b'=>25,'c'=>[1,3,5]];
 
-// Borrar una cookie: poniendo la funcion de la hora en pasado, en negativo 
+//con serialize conviertes un array en una cadena. Es un método propio de php 
+$infoguardar = serialize($miarray);
+
+$infojson = json_encode($miarray);
+
+echo "<br> Codificación del array en JSON: " .  $infojson;
+$micad = '{}';
+$infodeco = json_decode($micad);
+
+//setcookie("primeracookie", "aceptado", strtotime("+1 minute"));
+//setcookie("segundacookie", $infoguardar, time()+600);
+
+setcookie("primeracookie", $infoguardar, time() +300);
+setcookie("segundacookie", $infojson, time()+600);
+
+// Borrar una cookie: poniendo la funcion de la hora en pasado, en negativo  (elimina y la borra)
 setcookie("primeracookier", "aceptado", time() -120);
 
 //caduca la cookie, no la elimina

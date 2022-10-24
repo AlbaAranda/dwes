cf8475eb6a26f8c126a03d5abb66edec7427aa85
Alba Aranda <albaarandagomez@gmail.com>
2022-10-24 17:07:26 +0100

    modificacion ejercicios

--- data/ejercicios/apuntes/valida_ejerc-cookies.php
+++ data/ejercicios/apuntes/valida_ejerc-cookies.php
@@ -1,52 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h3>Ejercicio cookies: </h3>
+    
 
     <?php
-        if (isset($_GET['envio'])) {
-            if (!empty($_POST['idiomas'])) {
-                $idioma = $_POST['idiomas'];
-                if($idioma == "Espagnol"){
-                    echo "<br>Bienvenido querido usuario. ";
-                }
-                elseif($idioma == "Ingles"){
-                    echo "<br>Welcome dear user.";
-                }
-                else {
-                    echo "<br>Willkommen sehr geehrter Nutzer. ";
-                }
-            } 
-            else {
-                echo "<br> <h3>No has seleccionado ningún idioma</h3>";
+        if (isset($_POST['envio'])) {
+            if (!empty($_POST['idiomas']) && !isset($_COOKIE["idiomas"])) {
+                setcookie("idiosmas",$_POST["idiomas"],time()+3600);
+            }
+        
+
+            if(!empty($_POST["marca"]) && !isset($_COOKIE["marca"])){
+                setcookie("marca",$_POST["marca"],time()+3600);
+            }
+
+            if(!empty($_POST["idioma"]) && !empty($_POST["marca"])){
+                //Redirige a la pag visualizaciones
+                header("Location: visualizaciones.php");
+                exit(); // o die(); Para que no se ejecute ningun otro codigo
             }
         }
 
-        if (isset($_POST['envio'])) {
-            if (!empty($_POST['coches'])) {
-                $coche = $_POST['coches'];
-                if($coche == "Ford"){
-                    echo "<br>Marca del coche preferido " . $coche;
-                }
-                elseif($coche == "Renault"){
-                    echo "<br>Marca del coche preferido " . $coche;
-                }
-                else {
-                    echo "<br>Marca del coche preferido " . $coche;
-                }
-            } 
-            else {
-                echo "<br> <h3>No has seleccionado ninguna marca de coche</h3>";
-            }
+        else{
+            setcookie("idiomas",'',time()-72000);
+            setcookie("marca",'',time(), -7200);
+            header("Location: webcookies.html");
+            exit();
         }
     ?>
-</body>
-
-</html>
